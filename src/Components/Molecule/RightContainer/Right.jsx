420a75236b8ad3814b38e89ac63bde70c894435c
Tazochig <chigladzetazo@gmail.com>
2025-03-20 04:55:32 +0400

    kide importi

--- src/Components/Molecule/RightContainer/Right.jsx
+++ src/Components/Molecule/RightContainer/Right.jsx
@@ -2,7 +2,7 @@ import "./Right.css";
 import Button from "../../Atoms/Button/Button";
 import Summary from "../../Atoms/Summary/Summary";
 import List from "../List/List";
-import Brain from "../../../assets/images/iconoir_brain.svg";
+import iconBrain from '../../../assets/images/iconoir_brain.svg';
 import Chat from "../../../assets/Images/iconoir_chat-remove.svg";
 import Eye from "../../../assets/Images/iconoir_eye-empty.svg";
 import Flash from "../../../assets/Images/Vector.svg";
@@ -15,7 +15,7 @@ function Right() {
         <Summary />
         <div className="listebii">
           <List header="Reaction" digit="80" pic={Flash} />
-          <List header="Memory" digit="92" pic={Brain} />
+          <List header="Memory" digit="92" pic={iconBrain} />
           <List header="Verbal" digit="61" pic={Chat} />
           <List header="Visual" digit="73" pic={Eye} />
         </div>
